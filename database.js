85eb7d99b34f03bbf0f6f898d6d55a588aa71108
Ivan Tapia <ivan.tapia@diurvanconsultores.com>
2020-12-26 07:23:33 -0500

    Cambios para la certificacion

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
-/*
+
 mongoose
     .connect(process.env.mongodbconnection,{
         useUnifiedTopology: true,
@@ -10,7 +10,8 @@ mongoose
     )
     .then((db) => console.log('Db is connected'))
     .catch((err) => console.log(err));
-*/
+
+/*
 mongoose
     .connect("mongodb+srv://diurvan:4ZI8ubWsbnweqRca@diurvanmongodb.cl5na.mongodb.net/db-notifications?retryWrites=true&w=majority",{
         useUnifiedTopology: true,
@@ -20,3 +21,4 @@ mongoose
     )
     .then((db) => console.log('Db is connected'))
     .catch((err) => console.log(err));
+*/
